97ae6b877086ac5336ec7b1cfa377ae05b0cc66e
leeeee <2078344665@qq.com>
2024-07-20 21:38:18 +0800

    remove code

--- src/main/java/com/example/snowflakeinserttkmybatisdemo/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.snowflakeinserttkmybatisdemo.mapper;
-
-import com.example.snowflakeinserttkmybatisdemo.bean.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends tk.mybatis.mapper.common.Mapper<User>{
-}
